9b8e2fb13a837a68cd0939be78e609760038540e
Orban Mia <miaorban@gmail.com>
2025-01-28 22:46:20 +0100

    sanitize form data

--- app/fitcheck/api/route.js
+++ app/fitcheck/api/route.js
@@ -17,22 +17,31 @@ const transporter = nodemailer.createTransport({
 export const POST = async (req) => { 
   const formData = await req.formData();
 
+  const sanitize = (str) => str.replace(/[^\w\s]/gi, '');
+
+  const email = sanitize(formData.get('email'));
+  const name = sanitize(formData.get('name'));
+  const babyAge = sanitize(formData.get('babyAge'));
+  const babyWeight = sanitize(formData.get('babyWeight'));
+  const carrierType = sanitize(formData.get('carrierType'));
+  const description = sanitize(formData.get('description'));
+
   const attachments = []
 
-    // Get all image Files from form data
-    for(const pair of formData.entries())
-    {
-      if (pair[1] instanceof File) {
-        const attachment = {
-          filename: pair[1].name,
-          // Use buffer to retrieve data from file
-          content: Buffer.from(await pair[1].arrayBuffer()),
-          contentType: pair[1].type
-        };
-        
-        attachments.push(attachment);
-      }
+  // Get all image Files from form data
+  for(const pair of formData.entries())
+  {
+    if (pair[1] instanceof File) {
+      const attachment = {
+        filename: pair[1].name,
+        // Use buffer to retrieve data from file
+        content: Buffer.from(await pair[1].arrayBuffer()),
+        contentType: pair[1].type
+      };
+      
+      attachments.push(attachment);
     }
+  }
 
   const referenceId = Math.random().toString(36).substring(7);
   const mailOptions = {
@@ -40,13 +49,13 @@ export const POST = async (req) => {
     to: 'miaorban@gmail.com',
     subject: 'Form Submission with Files',
     html: `
-            <p>Email: ${formData.get('email')} </p>
+            <p>Email: ${email} </p>
             <p>referenceId: ${referenceId} </p>
-            <p>Name: ${formData.get('name')} </p>
-            <p>babyAge: ${formData.get('babyAge')} </p>
-            <p>babyWeight: ${formData.get('babyWeight')} </p>
-            <p>carrierType: ${formData.get('carrierType')} </p>
-            <p>description: ${formData.get('description')} </p>
+            <p>Name: ${name} </p>
+            <p>babyAge: ${babyAge} </p>
+            <p>babyWeight: ${babyWeight} </p>
+            <p>carrierType: ${carrierType} </p>
+            <p>description: ${description} </p>
             `,
     attachments
   };
