be493dae0d798608b156766d9ba4e07a5e6a9292
Orban Mia <miaorban@gmail.com>
2025-01-28 22:04:29 +0100

    log fitcheck error

--- app/fitcheck/page.tsx
+++ app/fitcheck/page.tsx
@@ -20,6 +20,7 @@ export default function FitCheck() {
       files.forEach((file, index) => {
         formData.append(`file${index}`, file);
       });
+      console.log('formData ', formData);
       
       const res = await fetch('/fitcheck/api/', {
         method: 'POST',
