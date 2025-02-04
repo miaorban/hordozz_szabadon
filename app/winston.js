53bce9d6c2bd780e20633cca4ecfb8253c07c6d0
Orban Mia <miaorban@gmail.com>
2025-02-04 14:44:08 +0100

    remove cloud logging

--- app/winston.js
+++ app/winston.js
@@ -1,20 +1,20 @@
 
-import { LoggingWinston } from "@google-cloud/logging-winston";
+// import { LoggingWinston } from "@google-cloud/logging-winston";
 import winston from "winston";
 
-const loggingWinston = new LoggingWinston({
-  projectId: process.env.GCP_PROJECT_ID,
-  credentials: {
-    client_email: process.env.GCP_SA_CLIENT_EMAIL,
-    private_key: process.env.GCP_PRIVATE_KEY.split(`\n`).join('\n'),
-  }
-});
+// const loggingWinston = new LoggingWinston({
+//   projectId: process.env.GCP_PROJECT_ID,
+//   credentials: {
+//     client_email: process.env.GCP_SA_CLIENT_EMAIL,
+//     private_key: process.env.GCP_PRIVATE_KEY.split(`\n`).join('\n'),
+//   }
+// });
 
 const logger = winston.createLogger({
   level: 'info',
   transports: [
     new winston.transports.Console(),
-    loggingWinston,
+    // loggingWinston,
   ],
 });
 
