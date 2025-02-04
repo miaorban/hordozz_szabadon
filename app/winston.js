f3e84ab925092574250c0f90b657d661efb4c7e4
Orban Mia <miaorban@gmail.com>
2025-02-04 14:50:10 +0100

    log loggingWinston

--- app/winston.js
+++ app/winston.js
@@ -1,14 +1,14 @@
 
-// import { LoggingWinston } from "@google-cloud/logging-winston";
+import { LoggingWinston } from "@google-cloud/logging-winston";
 import winston from "winston";
 
-// const loggingWinston = new LoggingWinston({
-//   projectId: process.env.GCP_PROJECT_ID,
-//   credentials: {
-//     client_email: process.env.GCP_SA_CLIENT_EMAIL,
-//     private_key: process.env.GCP_PRIVATE_KEY.split(`\n`).join('\n'),
-//   }
-// });
+const loggingWinston = new LoggingWinston({
+  projectId: process.env.GCP_PROJECT_ID,
+  credentials: {
+    client_email: process.env.GCP_SA_CLIENT_EMAIL,
+    private_key: process.env.GCP_PRIVATE_KEY.split(`\n`).join('\n'),
+  }
+});
 
 const logger = winston.createLogger({
   level: 'info',
@@ -18,4 +18,5 @@ const logger = winston.createLogger({
   ],
 });
 
+logger.info('loggingWinston', { loggingWinston})
 export { logger };
