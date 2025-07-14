fe96621bc0a6b0adb4a15a0e2cc3ceb94ac8d5d6
Orban Mia <miaorban@gmail.com>
2025-07-14 21:38:11 +0200

    change storage authentication mode

--- app/fitcheck/file-uploader/api/route.js
+++ app/fitcheck/file-uploader/api/route.js
@@ -3,6 +3,28 @@ export const dynamic = "force-dynamic";
 import { Storage } from '@google-cloud/storage';
 import { NextResponse } from 'next/server';
 import { logger } from '@/app/winston';
+import { getVercelOidcToken } from '@vercel/functions/oidc';
+import { ExternalAccountClient } from 'google-auth-library';
+
+const GCP_PROJECT_ID = process.env.GCP_PROJECT_ID;
+const GCP_PROJECT_NUMBER = process.env.GCP_PROJECT_NUMBER;
+const GCP_SERVICE_ACCOUNT_EMAIL = process.env.GCP_SERVICE_ACCOUNT_EMAIL;
+const GCP_WORKLOAD_IDENTITY_POOL_ID = process.env.GCP_WORKLOAD_IDENTITY_POOL_ID;
+const GCP_WORKLOAD_IDENTITY_POOL_PROVIDER_ID =
+  process.env.GCP_WORKLOAD_IDENTITY_POOL_PROVIDER_ID;
+  
+// Initialize the External Account Client
+const authClient = ExternalAccountClient.fromJSON({
+  type: 'external_account',
+  audience: `//iam.googleapis.com/projects/${GCP_PROJECT_NUMBER}/locations/global/workloadIdentityPools/${GCP_WORKLOAD_IDENTITY_POOL_ID}/providers/${GCP_WORKLOAD_IDENTITY_POOL_PROVIDER_ID}`,
+  subject_token_type: 'urn:ietf:params:oauth:token-type:jwt',
+  token_url: 'https://sts.googleapis.com/v1/token',
+  service_account_impersonation_url: `https://iamcredentials.googleapis.com/v1/projects/-/serviceAccounts/${GCP_SERVICE_ACCOUNT_EMAIL}:generateAccessToken`,
+  subject_token_supplier: {
+    // Use the Vercel OIDC token as the subject token
+    getSubjectToken: getVercelOidcToken,
+  },
+});
 
 // Creates a client
 const storage = process.env.NODE_ENV === 'development' 
@@ -10,7 +32,10 @@ const storage = process.env.NODE_ENV === 'development'
       projectId: "hordozz-szabadon",
       keyFilename: "hordozz-szabadon-4ee8806bc94e.json"
     })
-  : new Storage();
+  : new Storage({
+    projectId: GCP_PROJECT_ID,
+    authClient
+  });
 
 export async function GET(request) {
     const { searchParams } = new URL(request.url);
