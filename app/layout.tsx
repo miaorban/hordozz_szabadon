5423733e5875668c5802b3da636473f43314c7ae
Orban Mia <miaorban@gmail.com>
2025-07-28 22:04:19 +0200

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { montserrat } from '@/app/fonts';
 import Head from "next/head";
 import Navbar from '@/app/components/layout/navbar';
 import Footer from "@/app/components/layout/footer";
-import CalendarFloatingIcon from "@/app/components/layout/CalendarFloatingIcon";
+// import CalendarFloatingIcon from "@/app/components/layout/CalendarFloatingIcon";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import HotjarInit from "./hotjarInit";
